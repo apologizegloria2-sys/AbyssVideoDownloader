4d6e3dcb2c24ca47bf964b1e0e2e89063d0e6fad
apologizegloria2-sys <apologize.gloria2@gmail.com>
2026-06-16 12:03:01 +0700

    Update VideoDownloader.kt

--- src/main/kotlin/com/abmo/services/VideoDownloader.kt
+++ src/main/kotlin/com/abmo/services/VideoDownloader.kt
@@ -170,7 +170,7 @@ class VideoDownloader: KoinComponent {
         Logger.debug("decodedDatas : ${decodedDatas}")
         val mediaMetadata = decodedDatas.toObject<Datas>()
         val encryptedMediaMetadata = mediaMetadata.media
-        val encryptedMediaMetadataSize = encryptedMediaMetadata.length
+        //val encryptedMediaMetadataSize = encryptedMediaMetadata.length
         Logger.debug("encryptedMediaMetadata : ${encryptedMediaMetadata}")
         //Logger.debug("encryptedMediaMetadataSize : ${encryptedMediaMetadataSize}")
 
@@ -191,7 +191,7 @@ class VideoDownloader: KoinComponent {
                 slug = mediaMetadata.slug,
                 md5_id = mediaMetadata.md5_id
             )
-        val mediaSourcesSize = mediaSources.length
+        //val mediaSourcesSize = mediaSources.length
         Logger.debug("mediaSources : ${mediaSources}")
         //Logger.debug("mediaSourcesSize : ${mediaSourcesSize}")
         return mediaSources
